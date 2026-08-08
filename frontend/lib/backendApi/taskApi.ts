90d7706d7ec8e2bb21e80c4884a862d18306422d
Azzazy6269 <mohamadelazzazy@gmail.com>
2026-08-08 04:37:35 +0300

    feat: implemented all tasks backend apis

--- frontend/lib/backendApi/taskApi.ts
+++ frontend/lib/backendApi/taskApi.ts
@@ -1,12 +1,35 @@
 import api from "./api";
-import type { Task, TaskResponse, TasksResponse } from "@/types/api";
+import type {
+    Task, 
+    getTaskResponse, 
+    getTasksResponse, 
+    CreateTaskRequest, 
+    CreateTaskResponse,
+    PatchTaskRequest,
+    PatchTaskResponse,
+    DeleteTaskResponse 
+} from "@/types/api";
 
 export async function getTasksFromBackend(): Promise<Task[]> {
-  const response = await api.get<TasksResponse>("/tasks");
+  const response = await api.get<getTasksResponse>("/tasks");
   return response.data.data;
 }
 
-export async function updateTaskInBackend(taskId: string, completed: boolean): Promise<Task> {
-  const response = await api.patch<TaskResponse>(`/tasks/${taskId}`, { completed });
+export async function getTaskByIdFromBackend(taskId: string): Promise<Task> {
+  const response = await api.patch<getTaskResponse>(`/tasks/${taskId}`);
   return response.data.data;
+}
+
+export async function createTaskInBackend(taskId: string, data: CreateTaskRequest): Promise<Task> {
+  const response = await api.patch<CreateTaskResponse>(`/tasks/${taskId}`, data);
+  return response.data.data;
+}
+
+export async function updateTaskInBackend(taskId: string, data: PatchTaskRequest): Promise<Task> {
+  const response = await api.patch<PatchTaskResponse>(`/tasks/${taskId}`, data);
+  return response.data.data;
+}
+
+export async function deleteTaskFromBackend(taskId: string): Promise<DeleteTaskResponse> {
+  await api.delete(`/tasks/${taskId}`);
 }
--- frontend/types/api.ts
+++ frontend/types/api.ts
@@ -6,25 +6,46 @@ export type Task = {
   updatedAt: string;
 };
 
-export type ActivityLog = {
-  id: string;
-  action?: string;
-  info?: string;
-  when: string;
-};
-
-export type TasksResponse = {
+export type getTasksResponse = {
   data: Task[];
 };
 
-export type TaskResponse = {
+export type getTaskResponse = {
   data: Task;
 };
+
+export type CreateTaskRequest = {
+  title: string; // required, trimmed, non-empty
+  completed?: boolean; // default false
+};
+
+export type CreateTaskResponse = {
+  data: Task;
+};
+
+export type PatchTaskRequest = {
+  title?: string; // if provided, must be string and long enough after trim
+  completed?: boolean;
+};
+
+export type PatchTaskResponse = {
+  data: Task;
+};
+
+export type DeleteTaskResponse = void;
 
 export type ErrorResponse = {
   error?: {
     message?: string;
   };
+};
+
+
+export type ActivityLog = {
+  id: string;
+  action?: string;
+  info?: string;
+  when: string;
 };
 
 export type TaskFilter = "all" | "completed" | "pending";
